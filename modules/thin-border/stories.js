bdebca9d0067bdf50c1aa6ea0f00c974f8acfa0d
Hejx <531601727@qq.com>
2019-01-08 15:38:56 +0800

    Add thin-border module

--- modules/thin-border/stories.js
+++ modules/thin-border/stories.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import readme from './README.md';
 
+import './stories.css';
+
 const notes = { markdown: readme };
 
-storiesOf('@workplus-css/thin-border', module)
+storiesOf('Thin border', module)
   .add('Default story', () => (
-    <p>@workplus-css/thin-border</p>
+    <ul className="w-tb-top ul-item">
+      <li className="w-tb-bottom li-item">1</li>
+      <li className="w-tb-bottom li-item">2</li>
+      <li className="w-tb-bottom li-item">3</li>
+      <li className="w-tb-bottom li-item">4</li>
+      <li className="w-tb-bottom li-item">5</li>
+      <li className="w-tb-bottom li-item">6</li>
+    </ul>
   ), { notes });
